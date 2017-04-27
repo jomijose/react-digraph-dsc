5cb9cf1b211121ed4e72a746d6260822a506d1ce
Jose <424827@cognizant.com>
2017-04-27 17:53:52 +0530

    initail commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import './index.css';
+import GraphNew from './GraphNew';
+// import Graph from './Graph';
+
+ReactDOM.render(
+  <GraphNew />,
+  document.getElementById('root')
+);
